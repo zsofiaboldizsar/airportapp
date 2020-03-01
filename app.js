4a818a30a1ef4b16a141b50535a6f79dbaca7629
zsofia boldizsar <zsofia.boldizsar@hu.ibm.com>
2020-03-01 14:35:01 +0100

    move views folder into src

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
+const path = require('path');
 
 const getAirportsRouter = require('./src/routes/get-airports');
 const getFormRouter = require('./src/routes/get-form');
@@ -7,13 +8,14 @@ const getFormRouter = require('./src/routes/get-form');
 const PORT = process.env.PORT || '3000';
 const app = express();
 
-app.set('views', 'views');
+app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'pug');
 
 /*-----------------------------------------
    Add middlewares
    ------------------------------------------ */
 const middlewares = [
+    helmet(),
     express.urlencoded({extended: false}),
     express.json(),
 ];
