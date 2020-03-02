c266d53daced62668fc1da9eb87d5ad0a7ab99c6
zsofia boldizsar <zsofia.boldizsar@hu.ibm.com>
2020-03-02 11:41:23 +0100

    add html attributes for validation

--- src/routes/validator.js
+++ src/routes/validator.js
@@ -3,13 +3,13 @@ const { body, validationResult } = require('express-validator');
 const formValidationRules = () => {
     return [
         body('lat').isLength({min:1}).withMessage('Required').bail()
-            .isDecimal({force_decimal: false, locale: "en-GB"}).withMessage('Must be a number between -90.00 and 90.00').bail(),
+            .isDecimal({force_decimal: false}).withMessage('Must be a number between -90.00 and 90.00').bail(),
 
         body('lon').isLength({min:1}).withMessage('Required.').bail()
-            .isDecimal({force_decimal: false, locale: "en-GB"}, ).withMessage('Must be a number between -180.00 and 180.00').bail(),
+            .isDecimal({force_decimal: false}).withMessage('Must be a number between -180.00 and 180.00').bail(),
 
         body('rad').isLength({min:1}).withMessage('Required').bail()
-            .isDecimal({force_decimal: false, locale: "en-GB"}).withMessage('Must be a number').bail(),
+            .isDecimal({force_decimal: false}).withMessage('Must be a number').bail(),
     ]
 };
 
